50a712ae6c8847c0324bdc0fba93a6cf9642638f
nobodytao <nobodytao@gmail.com>
2023-08-18 18:05:57 +0400

    Exercises for chapter 2.9 Functions

--- ex_9.1_functions.py
+++ ex_9.1_functions.py
@@ -8,6 +8,12 @@ access_mode_template = [
     "spanning-tree bpduguard enable",
 ]
 
+port_security_template = [
+    "switchport port-security maximum 2",
+    "switchport port-security violation restrict",
+    "switchport port-security"
+]
+
 dict_arg={'FastEthernet0/12': 10,
      'FastEthernet0/14': 11,
      'FastEthernet0/16': 17}
@@ -18,7 +24,7 @@ dict_arg2 = {
     "FastEthernet0/09": 107,
 }
 
-def accesslist(dictionary_with_interfaces,template_for_dictionary):
+def accesslist(dictionary_with_interfaces,template_for_dictionary,template_for_port=None):
     access_list = []
     for dict_key in dictionary_with_interfaces.keys():
         access_list.append("interface "+dict_key)
@@ -27,6 +33,12 @@ def accesslist(dictionary_with_interfaces,template_for_dictionary):
                 access_list.append(acc_item+" "+str(dictionary_with_interfaces[dict_key]))
             else: 
                 access_list.append(acc_item)
+        if  template_for_port:
+            for port_item in template_for_port:
+                access_list.append(port_item)
+            
     return access_list
 
 print(accesslist(dict_arg2,access_mode_template))
+print("")
+print(accesslist(dict_arg2,access_mode_template,port_security_template))
