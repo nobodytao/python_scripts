32e69cdb58910ecffea23221f5f80bb5366a67d0
nobodytao <nobodytao@gmail.com>
2023-08-18 17:52:10 +0400

    Exercises for chapter 2.9 Functions

--- ex_9.1_functions.py
+++ ex_9.1_functions.py
@@ -12,8 +12,15 @@ dict_arg={'FastEthernet0/12': 10,
      'FastEthernet0/14': 11,
      'FastEthernet0/16': 17}
 
-def accesslist(dictionary_with_interfaces):
+def accesslist(dictionary_with_interfaces,template_for_dictionary):
     access_list = []
+    for dict_key in dictionary_with_interfaces.keys():
+        access_list.append("interface "+dict_key)
+        for acc_item in template_for_dictionary:
+            if acc_item.startswith('switchport access vlan'):
+                access_list.append(acc_item+" "+str(dictionary_with_interfaces[dict_key]))
+            else: 
+                access_list.append(acc_item)
     return access_list
 
-print(accesslist(dict_arg))
+print(accesslist(dict_arg,access_mode_template))
