e79cbaaca539bc72164c5be87f0a4c6851925daa
nobodytao <nobodytao@gmail.com>
2023-08-18 17:21:45 +0400

    Exercises for chapter 2.9 Functions

--- ex_9.1_functions.py
+++ ex_9.1_functions.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
 
+access_mode_template = [
+    "switchport mode access",
+    "switchport access vlan",
+    "switchport nonegotiate",
+    "spanning-tree portfast",
+    "spanning-tree bpduguard enable",
+]
+
+dict_arg={'FastEthernet0/12': 10,
+     'FastEthernet0/14': 11,
+     'FastEthernet0/16': 17}
+
+def accesslist(dictionary_with_interfaces):
+    access_list = []
+    return access_list
+
+print(accesslist(dict_arg))
