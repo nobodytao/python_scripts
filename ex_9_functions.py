5521d647d8aadc5944acaba892d62888a1355e4b
nobodytao <nobodytao@gmail.com>
2023-08-18 16:56:33 +0400

    Exercises for chapter 2.9 Functions

--- ex_9_functions.py
+++ ex_9_functions.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
-def summ(a,b):
+def summ(a,b,c='Сумма: '):
     '''
     This funtion summarize two integers, then two strings.
     Return a tuple with (a+b,str(a)+str(b))
     '''
-    return a+b, str(a)+str(b)
+    return c, a+b, str(a)+str(b)
 
 print(summ(1,2))
 print(summ(b=1,a=2))
+print(summ(1,2,c='Theese are digits'))
+
+def summ_unlimit(*args):
+    return len(args)
+
+print(summ_unlimit(1,2,3,4,5,6,7,8,9,10))
+
+def summ_kw(**kwargs):
+    return len(kwargs)
+
+print(summ_kw(a1=1,a2=2,a3=3))
