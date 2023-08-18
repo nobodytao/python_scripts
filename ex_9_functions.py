78a656699861913d1ddd9e07fd155b6a46b13d4c
nobodytao <nobodytao@gmail.com>
2023-08-18 16:07:30 +0400

    Started chapter 2.9

---
+++ ex_9_functions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def summ(a,b):
+    '''
+    This funtion summarize two integers, then two strings.
+    Return a tuple with (a+b,str(a)+str(b))
+    '''
+    return a+b, str(a)+str(b)
+
+print(summ(1,2))
