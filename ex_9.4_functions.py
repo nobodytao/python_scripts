3783002849938335dfc4f9ba9921dfbcae94e3c4
nobodytao <nobodytao@gmail.com>
2023-08-22 20:33:05 +0400

    Exersize 9.4. Finished chapter 2.9

--- ex_9.4_functions.py
+++ ex_9.4_functions.py
@@ -21,27 +21,29 @@ resultdict={}
 
 def convert_config_to_dict(filename):
     with open(filename,'r') as config_file:
+        
         fileline = ''
-        NextCommand = ''
-        list2lvlcommands = ['0']
+        list2lvlcommands = []
+        resultlist=[]
+
         while not(fileline.startswith('end')):
             fileline = config_file.readline().strip("\n")
-            if not(fileline.startswith('!')):
-                if not(ignore_command(fileline, ignore)):
-                     
-                    if not(fileline.startswith(" ")):
-                       if (len(list2lvlcommands) == 0):
-                          resultdict[NextCommand]=list2lvlcommands    
-                       list2lvlcommands = []
-                       NextCommand = fileline
-                       
-                    if (fileline.startswith(" ")):
-                        while fileline.startswith(" "):
-                            if not(ignore_command(fileline, ignore)):
-                                list2lvlcommands.append(fileline.strip())
-                            fileline = config_file.readline().strip("\n")
-                        resultdict[NextCommand]=list2lvlcommands
-                    
-    return resultdict
+            if not(fileline.startswith('!')) and (not(ignore_command(fileline, ignore))):
 
+                if (fileline.startswith(" ")):
+                    list2lvlcommands.append(fileline.strip(" "))
+                    
+                else:
+                    resultlist.append(list2lvlcommands)
+                    list2lvlcommands = []
+                    resultlist.append(fileline)
+ 
+    resultlist.pop(len(resultlist)-1)
+    resultlist.pop(0)
+
+    for listindex in range (0,int(len(resultlist)),2):
+        resultdict[resultlist[listindex]]=resultlist[listindex+1]
+
+    return (resultdict)               
+               
 print(convert_config_to_dict('config_sw1.txt'))
