c67502606edaee7c2d91ca99a836af0340de382e
nobodytao <nobodytao@gmail.com>
2023-09-02 18:23:54 +0400

    Chapter 12 Useful modules

--- ex_12_usefulmodules.py
+++ ex_12_usefulmodules.py
@@ -1,5 +1,17 @@
-import subprocess
+import subprocess, os, ipaddress
 
-result = subprocess.run('ls')
+#standard input/output stream
+result = subprocess.run(['ls', '-ls'], stdout=subprocess.PIPE)
+print(result.stdout.decode('utf-8'))
 
-print(result)
+#Turn off standard input/output stream, only result
+result = subprocess.run(['ls', '-ls'], stdout=subprocess.DEVNULL)
+print(result.stdout)
+print(result.returncode)
+
+#Stderr
+result = subprocess.run(['ping', '-c', '3', '-n', 'a'], stderr=subprocess.PIPE, encoding='utf-8')
+print(result.stderr)
+
+print(os.path.abspath("python_scripts"))
+
