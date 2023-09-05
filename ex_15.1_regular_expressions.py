90a7445d2c3d38566dda07eae962f698ec35867a
nobodytao <nobodytao@gmail.com>
2023-09-05 21:13:48 +0400

    Chapter 15 regex, re

--- ex_15.1_regular_expressions.py
+++ ex_15.1_regular_expressions.py
@@ -1,14 +1,24 @@
 import re
 
 def get_ip_from_cfg(filename):
-    result_list = []
+    result_dict = {}
+    is_interface = True
+
     with open(filename,'r') as file_settings:
         for one_line in file_settings:
-            match = re.search(r'(\d+.\d+.\d+.\d+) (\d+.\d+.\d+.\d+)', one_line)
-            if (match):
-                result_list.append((match.group(1),match.group(2)))
-                print((match.group(1),match.group(2)))
 
-    return result_list
+            if (re.search(r'interface [a-zA-Z]+\S+\n', one_line)):
+                interface = re.search(r'(interface [a-zA-Z]+\S+)\n', one_line).group(1)
+                is_interface = True
+
+            if (re.search(r'!\n', one_line)):
+                is_interface = False
+
+            match = re.search(r'(\d+.\d+.\d+.\d+) (\d+.\d+.\d+.\d+)', one_line)
+            if (match) and (is_interface == True):
+                is_interface = False
+                result_dict[interface] = (match.group(1),match.group(2))
+
+    return result_dict
 
 print(get_ip_from_cfg('config_r1.txt'))
