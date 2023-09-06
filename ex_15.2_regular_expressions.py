ba64befdc7b4ce40c335eeed5d09e36fe3b2f5e3
nobodytao <nobodytao@gmail.com>
2023-09-06 11:20:16 +0400

    Chapter 15 exercises. Regex

--- ex_15.2_regular_expressions.py
+++ ex_15.2_regular_expressions.py
@@ -1,10 +1,25 @@
 import re
 
 def parse_sh_ip_int_br(filename):
+    interface = ''
+    ipaddress = ''
+    interf_status = ''
+    interf_protocol = ''
+    result_list = []
     with open(filename,'r') as file_with_commands:
         for one_line in file_with_commands:
-            pass
+            if (re.search(r'\S+\d/*\d* ', one_line)):
+                match = re.search(r'(\S+\d/*\d*/*\d*) +((\d+\.\d+\.\d+\.\d+)|(unassigned))( +\S+){2} +(\S+ ?\S*) +(\S+)', one_line)
+
+                interface = match.group(1)
+                ipaddress = match.group(2)
+                interf_status = match.group(6)
+                interf_protocol = match.group(7)
+
+                result_list.append((interface, ipaddress, interf_status, interf_protocol))
+
+    return result_list
 
 
 
-parse_sh_ip_int_br('sh_ip_int_br.txt')
+print(parse_sh_ip_int_br('sh_ip_int_br.txt'))
