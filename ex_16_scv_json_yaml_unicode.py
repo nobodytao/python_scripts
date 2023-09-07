0d162593f6af1201dcd8b21c290e976654d6a5f7
nobodytao <nobodytao@gmail.com>
2023-09-07 10:32:24 +0400

    Chapter 16 csv yaml json unicode

--- ex_16_scv_json_yaml_unicode.py
+++ ex_16_scv_json_yaml_unicode.py
@@ -1,4 +1,4 @@
-import subprocess 
+import subprocess, locale
 #print("\N{LATIN SMALL LETTER O WITH DIAERESIS}", ord('ö'), chr(246))
 #b1 = b'\xd0\xb4\xd0\xb0'
 #bytes2 = b'\x68\x65\x6c\x6c\x6f'
@@ -7,6 +7,12 @@ import subprocess
 #b4 = bytes.decode(b'\xd0\xb4', encoding='utf-8')
 #print(b1, bytes2, b3, b4)
 
-result = subprocess.run(['ping', '-c', '3', '-n', '8.8.8.8'], stdout=subprocess.PIPE)
+#result = subprocess.run(['ping', '-c', '3', '-n', '8.8.8.8'], stdout=subprocess.PIPE)
+#output = result.stdout.decode('utf-8')
+#print(type(result))
+#print(type(output))
 
-print(result)
+print(locale.getpreferredencoding())
+
+f = open('test_encoding.txt','w')
+print(f)
