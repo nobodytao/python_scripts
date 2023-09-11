eb7f4a3c55670c959ce1d419b384010135b10123
nobodytao <nobodytao@gmail.com>
2023-09-11 22:10:36 +0400

    Chapter 20 jinja. Exercises

--- ex_20.3_jinja.py
+++ ex_20.3_jinja.py
@@ -3,8 +3,8 @@ import yaml, sys, os, ex_20_jinja
 
 '''MAIN'''
 if __name__ == "__main__":
-    data_file = "data_files/router_info.yml"
-    template_file = "templates/cisco_router_base.txt"
+    data_file = "data_files/ospf.yml"
+    template_file = "templates/ospf.txt"
     with open(data_file) as f:
         data = yaml.safe_load(f)
     print(ex_20_jinja.generate_config(template_file, data))
