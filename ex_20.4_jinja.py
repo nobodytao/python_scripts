e58aa0076a11440b26de2116c843712e0840f06c
nobodytao <nobodytao@gmail.com>
2023-09-12 11:56:30 +0400

    Chapter 20 jinja. Exercises

--- ex_20.4_jinja.py
+++ ex_20.4_jinja.py
@@ -3,8 +3,8 @@ import yaml, sys, os, ex_20_jinja
 
 '''MAIN'''
 if __name__ == "__main__":
-    data_file = "data_files/router_info.yml"
-    template_file = "templates/ospf.txt"
+    data_file = "data_files/add_vlan_to_switch.yaml"
+    template_file = "templates/add_vlan_to_switch.txt"
     with open(data_file) as f:
         data = yaml.safe_load(f)
     print(ex_20_jinja.generate_config(template_file, data))
